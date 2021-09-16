8134dd4c8b7bba4f93c8b178d1e499f711f1c273
min050410 <min050410@gmail.com>
2021-09-16 17:30:47 +0900

    server-9/16

---
+++ backup_in_server/comment_views.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from flask import Blueprint, url_for, request, render_template
+from werkzeug.utils import redirect
+
+from zelda import db
+from ..forms import CommentForm
+from zelda.models import Post, Comment
+
+bp = Blueprint('comment', __name__, url_prefix='/comment')
+
+
+@bp.route('/create/<int:post_id>', methods=('POST',))
+def create(post_id):
+  form = CommentForm()
+  post = Post.query.get_or_404(post_id)
+  if form.validate_on_submit():
+    content = request.form['content']
+    comment = Comment(content=content, create_date=datetime.now())
+    post.comment_set.append(comment)
+    db.session.commit()
+    return redirect(url_for('main.detail', post_id = post_id))
+  return render_template('post_detail.html', post=post, form=form)
+	#post = Post.query.get_or_404(post_id)
+	#content = request.form['content']
+	#comment = Comment(question=post, content=content, create_date=datetime.now())
+	#db.session.add(comment)
+	#db.session.commit()
+	#return redirect(url_for('main.detail', post_id = post_id))
