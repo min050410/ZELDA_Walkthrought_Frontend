e1eb56b0f4db004e450dcc6409233bae764ce297
min050410 <min050410@gmail.com>
2021-09-25 19:25:30 +0900

    9/25

--- backup_in_server/comment_views.py
+++ backup_in_server/comment_views.py
@@ -1,22 +1,25 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request, render_template
+from flask import Blueprint, url_for, request, render_template, g
 from werkzeug.utils import redirect
 
 from zelda import db
 from ..forms import CommentForm
 from zelda.models import Post, Comment
 
+from zelda.views.auth_views import Comment_login_required
+
 bp = Blueprint('comment', __name__, url_prefix='/comment')
 
 
 @bp.route('/create/<int:post_id>', methods=('POST',))
+@Comment_login_required
 def create(post_id):
   form = CommentForm()
   post = Post.query.get_or_404(post_id)
   if form.validate_on_submit():
     content = request.form['content']
-    comment = Comment(content=content, create_date=datetime.now())
+    comment = Comment(content=content, create_date=datetime.now(), user=g.user)
     post.comment_set.append(comment)
     db.session.commit()
     return redirect(url_for('main.detail', post_id = post_id))
--- backup_in_server/main_views.py
+++ backup_in_server/main_views.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, g
 from zelda.models import Post
 from ..forms import PostForm, CommentForm
 from werkzeug.utils import redirect
-from zelda import db  #수정
+from zelda import db  
+from zelda.views.auth_views import Post_login_required
 
 bp = Blueprint('main',__name__, url_prefix='/')
 
@@ -25,10 +26,11 @@ def detail(post_id): #매개변수에는 <int:post_id>가 들어감
   return render_template('post_detail.html', post=post, form=form)
  
 @bp.route('/create', methods=('GET', 'POST'))
+@Post_login_required
 def create(): #수정 
   form = PostForm()
   if request.method == 'POST' and form.validate_on_submit():
-    post = Post(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+    post = Post(subject=form.subject.data, content=form.content.data, create_date=datetime.now(), user=g.user)
     db.session.add(post)
     db.session.commit()
     return redirect(url_for('main.board'))
